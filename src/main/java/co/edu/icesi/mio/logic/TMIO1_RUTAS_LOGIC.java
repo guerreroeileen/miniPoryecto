70961f588441a1081a2e12040409483e75c56d7b
Eileen Guerrero <eileenguerrerogomez@gmail.com>
2018-10-31 07:14:29 -0500

    actualizacion de la clase tmio1_rutas_logic

--- src/main/java/co/edu/icesi/mio/logic/TMIO1_RUTAS_LOGIC.java
+++ src/main/java/co/edu/icesi/mio/logic/TMIO1_RUTAS_LOGIC.java
@@ -3,6 +3,8 @@ package co.edu.icesi.mio.logic;
 import java.math.BigDecimal;
 import java.util.List;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import co.edu.icesi.mio.dao.ITmio1_Rutas_DAO;
 import co.edu.icesi.mio.exceptions.LogicException;
 import co.edu.icesi.mio.model.Tmio1Ruta;
@@ -12,29 +14,64 @@ public class TMIO1_RUTAS_LOGIC implements ITMIO1_RUTAS_LOGIC{
 	private ITmio1_Rutas_DAO dao_rutas;
 
 	@Override
+	@Transactional(rollbackFor = LogicException.class)
 	public void save(Tmio1Ruta entity) throws LogicException {
 		if (entity!=null) {
 			if (entity.getId()!=null) {
-				Integer.parseInt(entity.getId());
+				
+			}else {
+				throw new LogicException();
 			}
 			
 		}else {
 			throw new LogicException();
 		}
 		
+		if (entity.getNumero()==null || entity.getNumero().length()!=3) {
+			throw new LogicException();
+		}
+		if (entity.getDescripcion()==null || entity.getDescripcion().length()>30){
+			throw new LogicException();
+		}
+		if (entity.getDiaInicio()==null || entity.getDiaFin()==null) {
+			throw new LogicException();
+		}
+		if (entity.getHoraFin()==null || entity.getHoraInicio()==null) {
+			throw new LogicException();
+		}
 	}
 
 	@Override
+	@Transactional(rollbackFor = LogicException.class)
 	public void update(Tmio1Ruta entity) throws LogicException {
 		if (entity!=null) {
+			if (entity.getId()!=null) {
+				
+			}else {
+				throw new LogicException();
+			}
 			
 		}else {
+			throw new LogicException();
+		}
+		
+		if (entity.getNumero()==null || entity.getNumero().length()!=3) {
+			throw new LogicException();
+		}
+		if (entity.getDescripcion()==null || entity.getDescripcion().length()>30){
+			throw new LogicException();
+		}
+		if (entity.getDiaInicio()==null || entity.getDiaFin()==null) {
+			throw new LogicException();
+		}
+		if (entity.getHoraFin()==null || entity.getHoraInicio()==null) {
 			throw new LogicException();
 		}
 		
 	}
 
 	@Override
+	@Transactional(rollbackFor = LogicException.class)
 	public void delete(Tmio1Ruta entity) throws LogicException {
 		if (entity!=null) {
 			
@@ -45,6 +82,7 @@ public class TMIO1_RUTAS_LOGIC implements ITMIO1_RUTAS_LOGIC{
 	}
 
 	@Override
+	@Transactional(rollbackFor = LogicException.class)
 	public List<Tmio1Ruta> findByRangeOfDays(BigDecimal diaInicio, BigDecimal diaFin) throws LogicException {
 		// TODO Auto-generated method stub
 		return null;
