c9fc276824566caab8fb926da03aa6c41fe07664
computacion <computacion@hgrid11.icesi.edu.co>
2018-11-01 17:08:05 -0500

    3 pruebas corriendo eb buses logica

--- src/main/test/co/edu/icesi/mio/testlogic/Test_TMIO1_BUSES_LOGIC.java
+++ src/main/test/co/edu/icesi/mio/testlogic/Test_TMIO1_BUSES_LOGIC.java
@@ -15,7 +15,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import co.edu.icesi.mio.exceptions.LogicException;
 import co.edu.icesi.mio.logic.ITMIO1_BUSES_LOGIC;
-import co.edu.icesi.mio.logic.TMIO1_BUSES_LOGIC;
 import co.edu.icesi.mio.model.Tmio1Bus;
 import co.edu.icesi.mio.model.Tmio1Servicio;
 import co.edu.icesi.mio.model.Tmio1ServiciosSitio;
@@ -31,16 +30,33 @@ public class Test_TMIO1_BUSES_LOGIC {
 	
 	
 	@Test
-	public void testPlacaMayor6Elementos() throws LogicException {
+	public void testAgregadoCorrecto () {
 		Tmio1Bus bus = new Tmio1Bus();
-		bus.setPlaca("SDF 457");
+		bus.setPlaca("SDF457");
 		bus.setCapacidad(new BigDecimal(1000));
 		bus.setMarca("Renault");
 		bus.setModelo(new BigDecimal(2015));
 		bus.setTipo("T");
 		bus.setTmio1Servicios(new ArrayList<Tmio1Servicio>());
 		bus.setTmio1ServiciosSitios(new ArrayList<Tmio1ServiciosSitio>());
-		
+		try {
+			buses_logica.save(bus);
+			fail("se agrego");
+		} catch (LogicException e) {
+			assertEquals("error",e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testPlacaMayor6Elementos() throws LogicException {
+		Tmio1Bus bus = new Tmio1Bus();
+		bus.setPlaca("SDF 4578");
+		bus.setCapacidad(new BigDecimal(1000));
+		bus.setMarca("Renault");
+		bus.setModelo(new BigDecimal(2015));
+		bus.setTipo("T");
+		bus.setTmio1Servicios(new ArrayList<Tmio1Servicio>());
+		bus.setTmio1ServiciosSitios(new ArrayList<Tmio1ServiciosSitio>());
 		
 		try {
 			buses_logica.save(bus);
@@ -50,6 +66,25 @@ public class Test_TMIO1_BUSES_LOGIC {
 		}
 		
 				
+	}
+	
+	
+	@Test
+	public void testMarcaNull () {
+		Tmio1Bus bus = new Tmio1Bus();
+		bus.setPlaca("SDF578");
+		bus.setCapacidad(new BigDecimal(1000));
+		bus.setModelo(new BigDecimal(2015));
+		bus.setTipo("T");
+		bus.setTmio1Servicios(new ArrayList<Tmio1Servicio>());
+		bus.setTmio1ServiciosSitios(new ArrayList<Tmio1ServiciosSitio>());
+		
+		try {
+			buses_logica.save(bus);
+			fail("se agrego");
+		} catch (LogicException e) {
+			assertEquals("error",e.getMessage());
+		}
 	}
 	
 	
